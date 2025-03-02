1b6684729cea4afeb6611aa06899373dcaa4dc70
Aswini-Anthonippillai <aswinianthonippillai@gmail.com>
2025-03-02 14:25:57 +0530

    Add files via upload

---
+++ CT_2021_083/LW_01/src/Q_01/Question_01.java
@@ -0,0 +1,17 @@
+package Q_01;
+import javax.swing.*;
+
+
+public class Question_01 {
+    public static void main(String[] args) {
+        JFrame myWindow;
+        myWindow = new JFrame();
+        myWindow.setSize(800,600);
+        myWindow.setTitle("Welcome to Java");
+        myWindow.setVisible(true);
+
+    }
+}
+
+
+
